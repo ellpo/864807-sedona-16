825d7a6adea6f9b328d73e0976e4e0b69f5c8faf
ellpo <elnara.popova@yandex.com>
2019-05-25 12:53:26 +0300

    завершение резиновой верстки

---
+++ source/js/script.js
@@ -0,0 +1,20 @@
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".page-header__toggle");
+var close = document.querySelector(".main-nav__close");
+
+navMain.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+  } else {
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+  }
+});
+
+close.addEventListener("click", function() {
+  navMain.classList.remove("main-nav--opened");
+  navMain.classList.add("main-nav--closed");
+});
