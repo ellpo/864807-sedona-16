3d786f636f14178e3405435503f130a402b4d7eb
ellpo <elnara.popova@yandex.com>
2019-05-29 17:33:34 +0300

    скорректировала меню

--- source/js/script.js
+++ source/js/script.js
@@ -3,18 +3,12 @@ var navToggle = document.querySelector(".page-header__toggle");
 var close = document.querySelector(".main-nav__close");
 
 navMain.classList.remove("main-nav--nojs");
+navToggle.classList.remove("page-header--nojs");
 
 navToggle.addEventListener("click", function() {
-  if (navMain.classList.contains("main-nav--closed")) {
-    navMain.classList.remove("main-nav--closed");
-    navMain.classList.add("main-nav--opened");
-  } else {
-    navMain.classList.add("main-nav--closed");
-    navMain.classList.remove("main-nav--opened");
-  }
+  navMain.classList.add("main-nav--opened");
 });
 
 close.addEventListener("click", function() {
   navMain.classList.remove("main-nav--opened");
-  navMain.classList.add("main-nav--closed");
 });
