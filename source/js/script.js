359d7d6f72113c413012ca61412d0fb31cbb8dbf
ellpo <elnara.popova@yandex.com>
2019-05-29 19:45:26 +0300

    внесла корректировки часть2

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,10 @@
+var pageHeader = document.querySelector(".page-header");
 var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".page-header__toggle");
 var close = document.querySelector(".main-nav__close");
 
+pageHeader.classList.remove("page-header--nojs");
 navMain.classList.remove("main-nav--nojs");
-navToggle.classList.remove("page-header--nojs");
 
 navToggle.addEventListener("click", function() {
   navMain.classList.add("main-nav--opened");
